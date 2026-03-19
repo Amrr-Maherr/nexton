3ed490b3c371d372b5522b3d25aba151435dfa9c
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-03-19 17:20:44 +0200

    feat: add auth functions.ts with placeholder methods

---
+++ app/auth/functions.ts
@@ -0,0 +1,19 @@
+export function login() {
+  // TODO: Implement login logic
+}
+
+export function register() {
+  // TODO: Implement register logic
+}
+
+export function logout() {
+  // TODO: Implement logout logic
+}
+
+export function forgotPassword() {
+  // TODO: Implement forgot password logic
+}
+
+export function resetPassword() {
+  // TODO: Implement reset password logic
+}
