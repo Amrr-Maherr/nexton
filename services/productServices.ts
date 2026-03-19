bb3ce23ffd47b4e6ee7b808c6d92bf7a722ad91d
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-03-19 17:48:46 +0200

    feat: add products API integration

---
+++ services/productServices.ts
@@ -0,0 +1,17 @@
+import { ProductsResponse } from './../types/product';
+import axios from "axios";
+
+const BaseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+const ProductsEndPoint = process.env.NEXT_PUBLIC_PRODUCTS_ENDPOINT;
+
+export const GetAllProducts = async (Filters?: Record<string, string>): Promise<ProductsResponse> => {
+    try {
+        const response = await axios.get<ProductsResponse>(`${BaseUrl}${ProductsEndPoint}`, {
+            params: Filters
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
---
+++ types/product.ts
@@ -0,0 +1,55 @@
+export interface Subcategory {
+  _id: string;
+  name: string;
+  slug: string;
+  category: string;
+}
+
+export interface Category {
+  _id: string;
+  name: string;
+  slug: string;
+  image: string;
+}
+
+export interface Brand {
+  _id: string;
+  name: string;
+  slug: string;
+  image: string;
+}
+
+export interface Product {
+  sold: number | null;
+  images: string[];
+  subcategory: Subcategory[];
+  ratingsQuantity: number;
+  _id: string;
+  title: string;
+  slug: string;
+  description: string;
+  quantity: number;
+  price: number;
+  priceAfterDiscount?: number;
+  imageCover: string;
+  category: Category;
+  brand: Brand;
+  ratingsAverage: number;
+  createdAt: string;
+  updatedAt: string;
+  id: string;
+  availableColors?: string[];
+}
+
+export interface ProductsMetadata {
+  currentPage: number;
+  numberOfPages: number;
+  limit: number;
+  nextPage?: number;
+}
+
+export interface ProductsResponse {
+  results: number;
+  metadata: ProductsMetadata;
+  data: Product[];
+}
